550f8c5ccda3dd5c6cd5547fac586a50098ce48c
kevinlam0 <kevinclam02@gmail.com>
2025-03-29 19:28:15 -0400

    Updating the needed configs for the rest of the app

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -5,11 +5,21 @@ dotenv.config();
 interface Config {
   port: number;
   nodeEnv: string;
+  clientId: string;
+  clientSecret: string;
+  redirectUri: string;
+  scope: string;
+  oauthUrl: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
-  nodeEnv: process.env.NODE_ENV || 'development'
+  nodeEnv: process.env.NODE_ENV || 'development',
+  clientId: process.env.KROGER_CLIENT_ID || '',
+  clientSecret: process.env.KROGER_CLIENT_SECRET || '',
+  redirectUri: process.env.KROGER_REDIRECT_URI || '',
+  scope: process.env.KROGER_SCOPE || 'product.compact',
+  oauthUrl: process.env.KROGER_OAUTH_URL || 'https://api.kroger.com/v1/connect/oauth2',
 };
 
 export default config;
