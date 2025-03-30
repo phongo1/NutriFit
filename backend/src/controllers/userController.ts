b451c7923f981f295dd6d3adf14b4e4419e9bdc7
raymondchen10 <646raymond@gmail.com>
2025-03-30 04:45:04 -0400

    createuser loginuser

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,12 +1,49 @@
 import { Request, Response, NextFunction } from 'express';
-// Import the user model
+import User from '../models/User';
+import { collections } from '../services/database.service';
 
-async function createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-  // TODO: stub
+export async function createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+try {
+
+  const { firstName, lastName, username, password } = req.body;
+
+  const existingUser = await collections.users?.findOne({ username });
+  if (existingUser) {
+    res.status(400).json({ error: "Username already exists"});
+    return;
+  }
+
+  const newUser = new User(firstName, lastName, username, password);
+  const result = await collections.users?.insertOne(newUser);
+  const user = await collections.users?.findOne({ _id: result?.insertedId });
+
+  res.status(201).json({ 
+    userId: result?.insertedId,
+  
+  });
+} catch (error) {
+  console.error("Error creating user:", error);
+  res.status(500).send("Failed to create user");
+}
 }
 
-async function loginUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-  // TODO: stub
+
+
+
+export async function loginUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+ try {
+  const {username, password} = req.body;
+
+  const user = await collections.users?.findOne({ username});
+if(!user || user.password !== password){
+  res.status(401).json({ error: "Invalid username or password"});
+  return;
+}
+  res.status(200).json({ message: "Login successful", userId: user._id });
+ } catch (error) {
+  console.error("Error loggin in:", error);
+  res.status(500).send("Login failed");
+ }
 }
 
 async function getSavedItems(req: Request, res: Response, next: NextFunction): Promise<void> {
