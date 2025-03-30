038355c7a7dd6109f151eb9afa6754777f603b45
kevinlam0 <kevinclam02@gmail.com>
2025-03-29 22:49:56 -0400

    Created main controller for the grocery

--- backend/src/controllers/groceryController.ts
+++ backend/src/controllers/groceryController.ts
@@ -1,1 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
+
+export const searchItem = (req: Request, res: Response, next: NextFunction) => {
+
+    const { searchTerm } = req.body;
+    // use the search term in the kroger search filter.term=milk
+    // limit the search to about 20-50 filter.limit=1-50
+    // brand filter.brand=brand
+
+    // get the price
+    // get the upc x
+}
